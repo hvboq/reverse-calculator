0050b452fef2ecd81c574a4615e94713c14941f4
hvboq <hvboq11@gmail.com>
2024-03-13 02:33:53 +0900

    [ADD] Temp Data

--- reverse_calculator/lib/model/main_model.dart
+++ reverse_calculator/lib/model/main_model.dart
@@ -1,23 +1,71 @@
 import 'package:reverse_calculator/model/character_model.dart';
 
 class MainModel {
-  Map<int, CharacterModel> characterModelMap = {
-    0: CharacterModel(
+  String imgUriBase =
+      "https://i.namu.wiki/i/oKtKgSm68oDwZnpjVqLbM4CWGSfRKWhSEJAm-lvyVBa3KmObRO-_2OGf88r2v7XomNZWEwF4DtsvU8Gj87nYk3rGSvVxRvhy7OokBx92GBkDMUlhtD0Gdm9A2nFR0m9mkiiqMtkFBGiwuDddxvTIVw.webp";
+
+  List<CharacterModel> characterModelMap = [
+    CharacterModel(
       idx: 0,
       name: "개발자",
-      imgUri: "셀카",
-    )
-  };
+      img: "54076361&range=B4",
+    ),
+    CharacterModel(
+      idx: 1,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 2,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 3,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 4,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 5,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 6,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 7,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 8,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+    CharacterModel(
+      idx: 9,
+      name: "갈라보나",
+      img: "54076361&range=B10",
+    ),
+  ];
 }
 
 class Model {
-  late int idx;
-  late String name;
-  late String imgUri;
+  final int idx;
+  final String name;
+  final String img;
 
   Model({
     required this.idx,
     required this.name,
-    required this.imgUri,
+    required this.img,
   });
 }
